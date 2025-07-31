b8e23b05bbb02de966c1a418983b4f765ecfbaf8
larissa <rissapins@gmail.com>
2025-07-31 14:50:18 -0300

    refactor: atualiza o ambiente para Prod

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
-import { DevService } from './data/services/dev.service';
+import { ProdService } from './data/services/prod.service';
 import { PostagemModule } from './postagem/postagem.module';
 import { TemaModule } from './tema/tema.module';
 import { UsuarioModule } from './usuario/usuario.module';
@@ -11,7 +11,7 @@ import { UsuarioModule } from './usuario/usuario.module';
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({
-      useClass: DevService, // alterar para DevService para desenvolvimento e DB MySQL ou ProdService para produção
+      useClass: ProdService, // alterar para DevService para desenvolvimento e DB MySQL ou ProdService para produção
       imports: [ConfigModule],
     }),
     PostagemModule,
