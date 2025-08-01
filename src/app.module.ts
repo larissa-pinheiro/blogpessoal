ba3a5b4bf3762c635adfdcee524984712c1aa442
larissa <rissapins@gmail.com>
2025-07-31 21:42:01 -0300

    fix: ajusta ambiente para produção

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
-import { DevService } from './data/services/dev.service';
+import { ProdService } from './data/services/prod.service';
 import { PostagemModule } from './postagem/postagem.module';
 import { TemaModule } from './tema/tema.module';
 import { UsuarioModule } from './usuario/usuario.module';
@@ -12,7 +12,7 @@ import { UsuarioModule } from './usuario/usuario.module';
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({
-      useClass: DevService, // alterar para DevService para desenvolvimento e DB MySQL ou ProdService para produção
+      useClass: ProdService, // alterar para DevService para desenvolvimento e DB MySQL ou ProdService para produção
       imports: [ConfigModule],
     }),
     PostagemModule,
